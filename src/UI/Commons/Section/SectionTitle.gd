cdffdd74ecfe3945eb16b87ba4cc2e46298733a0
levrault <lucf.langis@gmail.com>
2021-12-30 17:21:26 -0500

    SECTION should keep data when app is running

--- src/UI/Commons/Section/SectionTitle.gd
+++ src/UI/Commons/Section/SectionTitle.gd
@@ -4,5 +4,10 @@ extends VBoxContainer
 export var text := "" setget _set_text
 
 
+func _ready() -> void:
+	$Label.text = text
+
+
 func _set_text(value: String) -> void:
+	text = value
 	$Label.text = value
