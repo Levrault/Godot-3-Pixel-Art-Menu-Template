e6712daf4651f0a0d09ff0824bc3e901db7df588
Luc-Frédéric Langis <lucf.langis@gmail.com>
2022-01-07 17:59:49 -0500

    Fix highlight behavior to behave better with mouse and other input (#10)

--- src/UI/Settings/Fields/FieldSet.gd
+++ src/UI/Settings/Fields/FieldSet.gd
@@ -2,6 +2,9 @@ class_name FieldSet, "res://assets/icons/fieldset.svg"
 extends Control
 
 var field = null
+var label: Label = null
+var description: Label = null
+var accessibleDescription: Label = null 
 
 onready var focus_rect = $FocusRect
 onready var container = $MarginContainer/FieldContainer
@@ -14,6 +17,8 @@ func _ready():
 
 	for child in container.get_children():
 		if not child.is_in_group("GameSettings"):
+			if child is Label:
+				label = child
 			continue
 		field = child
 
@@ -21,9 +26,14 @@ func _ready():
 		printerr("Field %s doesn't have any field" % get_name())
 		return
 
-	field.connect("mouse_entered", self, "_on_Mouse_entered")
+	field.connect("mouse_entered", self, "_on_Field_mouse_entered")
+	field.connect("mouse_exited", self, "_on_Field_mouse_exited")
+	label.connect("mouse_entered", self, "_on_Label_mouse_entered")
+	label.connect("mouse_exited", self, "_on_Label_mouse_exited")
 	Events.connect("field_focus_entered", self, "_on_Field_focus_entered")
+	Events.connect("field_focus_exited", self, "_on_Field_focus_exited")
 	focus_rect.modulate.a = 0.0
+	label.mouse_filter = MOUSE_FILTER_STOP
 
 
 func _on_Field_focus_entered(focused_field) -> void:
@@ -33,6 +43,24 @@ func _on_Field_focus_entered(focused_field) -> void:
 	focus_rect.modulate.a = 1.0
 
 
-func _on_Mouse_entered() -> void:
+func _on_Field_focus_exited(focused_field) -> void:
+	if focused_field != field:
+		return
+	print("%s has loss focus" % focused_field)
+	focus_rect.modulate.a = 0.0
+
+
+func _on_Field_mouse_entered() -> void:
 	Events.emit_signal("field_focus_entered", field)
-	focus_rect.modulate.a = 1.0
+
+
+func _on_Field_mouse_exited() -> void:
+	focus_rect.modulate.a = 0.0
+
+
+func _on_Label_mouse_entered() -> void:
+	Events.emit_signal("field_focus_entered", field)
+
+
+func _on_Label_mouse_exited() -> void:
+	focus_rect.modulate.a = 0.0
