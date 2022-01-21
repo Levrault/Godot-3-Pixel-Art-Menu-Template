72bf2d100174d56774ceab13cb7601b789f21047
levrault <lucf.langis@gmail.com>
2022-01-21 16:17:20 -0500

    Should set the correct local on first boot

--- src/UI/Settings/Updater/LocaleUpdater.gd
+++ src/UI/Settings/Updater/LocaleUpdater.gd
@@ -1,9 +1,22 @@
 extends Updater
 
 
-func apply(properties: Dictionary, trigger_callback_action := true) -> void:
-	print(Config.is_new)
-	TranslationServer.set_locale(properties.locale)
+func _ready() -> void:
+	yield(owner, "ready")
+	
+	if Config.is_new:
+		var os_locale = OS.get_locale().left(2)
+		if TranslationServer.get_loaded_locales().find(os_locale):
+			Events.connect("config_file_saved", self, "_on_Config_file_saved")
+			Config.save_field(owner.form.engine_file_section, get_parent().key, os_locale)
 
+
+func apply(properties: Dictionary, trigger_callback_action := true) -> void:
+	TranslationServer.set_locale(properties.locale)
 	if trigger_callback_action:
 		get_parent().save()
+
+
+func _on_Config_file_saved() -> void:
+	get_parent().revert()
+	Events.disconnect("config_file_saved", self, "_on_Config_file_saved")
