a8ea05198869f98ffd9c9e256ba674c456ec174f
levrault <lucf.langis@gmail.com>
2022-03-08 14:22:12 -0500

    NavigationButton now works in child scene of a NavigationSwitch

--- src/UI/Commons/Navigation/NavigationButton.gd
+++ src/UI/Commons/Navigation/NavigationButton.gd
@@ -7,10 +7,13 @@ extends Button
 export var navigate_to := ""
 export var is_default_focused := false
 
+var navigation_switch: NavigationSwitch = null
+
 
 # Focus itself if default focused and menu is visible
 func _ready() -> void:
 	yield(owner, "ready")
+	navigation_switch = find_navigation_switch(owner)
 	connect("pressed", self, "_on_Pressed")
 	connect("mouse_entered", self, "grab_focus")
 
@@ -20,11 +23,24 @@ func _ready() -> void:
 			grab_focus()
 
 
+# loop owner to find the NavigationSwitch
+# use to make navigation button compatible if
+# not a direct child of a NavigationSwitch
+func find_navigation_switch(node: Node) -> Node:
+	if node is NavigationSwitch:
+		return node
+
+	while (not node is NavigationSwitch):
+		node = node.owner
+
+	return node
+
+
 # Update Menu navigation history (Autoload/Menu.gd)
 # Set itself at last clicked button
 func _on_Pressed() -> void:
-	owner.last_clicked_button = self
-	Menu.history.append(owner.get_name())
+	navigation_switch.last_clicked_button = self
+	Menu.history.append(navigation_switch.get_name())
 	Menu.navigate_to(navigate_to)
 	Menu.current_route = navigate_to
-	print_debug("%s has change navigation history : %s" % [owner.get_name(), Menu.history])
+	print_debug("%s has change navigation history : %s" % [navigation_switch.get_name(), Menu.history])
