0b25b76d1d6792834ca681ca624d088b2b34dcbf
levrault <lucf.langis@gmail.com>
2022-07-14 17:12:56 -0400

    Fix an issue where changing game layout would merge both config

--- src/UI/Settings/Updater/GamepadLayoutUpdater.gd
+++ src/UI/Settings/Updater/GamepadLayoutUpdater.gd
@@ -1,9 +1,18 @@
 extends Updater
 
 const CUSTOM_LAYOUT = "custom"
+var previous_actions := []
 
 
 func apply(properties: Dictionary, trigger_callback_action := true) -> void:
+	
+	# clear previous config
+	for device in InputManager.all_gamepad_devices:
+		for action in previous_actions:
+			InputManager.removeJoyButtonEvent(action.key, action.value)
+			InputManager.removeJoyMotionEvent(action.key, action.value)
+	
+	# udpate
 	if properties.layout != CUSTOM_LAYOUT:
 		for device in InputManager.all_gamepad_devices:
 			for action in EngineSettings.gamepad[device][properties.layout]:
@@ -15,6 +24,9 @@ func apply(properties: Dictionary, trigger_callback_action := true) -> void:
 					InputManager.addJoyMotionEvent(action, value)
 				else:
 					InputManager.addJoyButtonEvent(action, value)
+					
+				# cache previous action
+				previous_actions.append({key = action, value = value})
 	else:
 		for device in InputManager.all_gamepad_devices:
 			for action in Config.values.gamepad_bindind[device]:
@@ -26,4 +38,8 @@ func apply(properties: Dictionary, trigger_callback_action := true) -> void:
 					InputManager.addJoyMotionEvent(action, value)
 				else:
 					InputManager.addJoyButtonEvent(action, value)
+					
+				# cache previous action
+				previous_actions.append({key = action, value = value})
+
 	Events.emit_signal("gamepad_layout_changed")
